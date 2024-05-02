b6844d538d21f29e93e25d4d03fc32685825cbd9
gracek <117389336+gracjan-p@users.noreply.github.com>
2024-05-02 21:31:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-function delay (URL) {
+
+function delay(URL) {
     const body = document.body;
     let elements = document.getElementsByClassName("portal");
     for (let i = 0; i < elements.length; i++) {
         setTimeout(() => {
-            elements[i].style.width = "200vw";
-            elements[i].style.height = "200vw";
-        });
-    }
-    setTimeout( function() { window.location = URL }, 1000 );
+            elements[i].style.width = "300vw";
+            elements[i].style.height = "300vw";
+        }, 0);}
+    setTimeout(() => {
+        window.location = URL;
+    }, 1000)
 }
 
-function delay2 (URL) {
-    const body = document.body;
-    let elements = document.getElementsByClassName("portal");
-    for (let i = 0; i < elements.length; i++) {
-        setTimeout(() => {
-            elements[i].style.backgroundColor = "var(--github)";
-            elements[i].style.width = "200vw";
-            elements[i].style.height = "200vw";
-        });
+window.addEventListener( "pageshow", function ( event ) {
+    let historyTraversal = event.persisted ||
+        ( typeof window.performance != "undefined" &&
+            window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+        // Handle page restore.
+        let elements = document.getElementsByClassName("portal");
+        for (let i = 0; i < elements.length; i++) {
+            setTimeout(() => {
+                elements[i].style.width = "0vw";
+                elements[i].style.height = "0vw";
+            }, 0);}
     }
-    setTimeout( function() { window.location = URL }, 1000 );
-}
+});
