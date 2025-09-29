e55141210c182cb16280dbb00d74c76bfec8e877
gracek <lekopyto2346@gmail.com>
2025-09-29 20:38:36 +0200

    new

---
+++ sub-websites/croppy/anim-trigger.js
@@ -0,0 +1,18 @@
+
+$('section').find('.img-wrapper').addClass('animate-on-scroll')
+$('section').find('.header-container').addClass('animate-on-scroll')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            $(entry.target).addClass('trigger-animation')
+        }
+    })
+}, {
+    rootMargin: '0px 0px -50px 0px',
+    threshold: 0.5
+})
+
+$('.animate-on-scroll').each(function () {
+    observer.observe(this)
+})
