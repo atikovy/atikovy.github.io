314c12994dfa711cfe18987ecea51918337d613f
gracek <lekopyto2346@gmail.com>
2025-10-08 15:16:53 +0200

    new

--- sub-websites/croppy/anim-trigger.js
+++ sub-websites/croppy/anim-trigger.js
@@ -1,6 +1,8 @@
 
 $('section').find('.img-wrapper').addClass('animate-on-scroll')
 $('section').find('.header-container').addClass('animate-on-scroll')
+$('.img-wrapper').find('img').addClass('animate-on-scroll')
+$('.img-wrapper').find('.leaf-shadow').addClass('animate-on-scroll')
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
